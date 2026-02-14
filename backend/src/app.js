2965aa855c6737332845442ba093b437d7f35c11
Sidhartha Paidi <paidisaisidhartha9@gmail.com>
2026-02-14 14:18:27 +0530

    routes for gig working tested with postman

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import cors from "cors"
 import userRoutes from "./routes/user.routes.js"
 import internshipRoutes from "./routes/internship.routes.js"
 import taskRoutes from "./routes/task.routes.js"
+import gigRoutes from "./routes/gig.routes.js"
 import adminRoutes from "./routes/admin/index.js"
 import mentorRoutes from "./routes/mentor/index.js"
 import publicInternshipRoutes from "./routes/public/internship.routes.js"
@@ -17,6 +18,7 @@ app.use(express.json())
 app.use("/api/users", userRoutes)
 app.use("/api/internships", internshipRoutes)
 app.use("/api/tasks", taskRoutes)
+app.use("/api/gigs", gigRoutes)
 app.use("/api/mentor", mentorRoutes)
 app.use("/api/admin", adminRoutes)
 app.use("/api/internships", publicInternshipRoutes)
